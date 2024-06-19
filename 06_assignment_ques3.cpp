e5d81ca046903dc7090de94da6ac3387425addd2
Krishnadhar Pandey <krishnadhargzp00@gmail.com>
2024-06-20 00:05:24 +0530

    sixth commit

---
+++ 06_assignment_ques3.cpp
@@ -0,0 +1,15 @@
+// print the array in the reverse form.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5];
+    for(int i=0;i<5;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=4;i>=0;i--)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
